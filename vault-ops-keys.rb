f7d6ce0a23a51e95506ad65d839e648cd2df12f9
clyons793 <christopher_lyons@comcast.com>
2022-11-27 18:32:34 -0500

    Updated

--- vault-ops-keys.rb
+++ vault-ops-keys.rb
@@ -1,7 +1,7 @@
 class VaultOpsKeys < Formula
   desc "Script to show keys from a list from Enterprise Vault instance"
   homepage "https://www.suncoast.systems/homebrew-sharedOps/"
-  url "https://github.com/dotcomrow/vault-ops-keys/archive/refs/tags/v1.0.tar.gz"
+  url "https://github.com/dotcomrow/vault-ops-keys/archive/refs/tags/v1.1.tar.gz"
   sha256 "1dab3cd2e654ff8de07353976ae87112391a8d31bf7d30e6e7b2d749f0766db0"
   license "MIT"
 
